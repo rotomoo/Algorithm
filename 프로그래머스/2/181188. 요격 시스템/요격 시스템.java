32759e5d29446c1bab47cb31086627749583487a
손병일 <30739181+rotomoo@users.noreply.github.com>
2024-11-28 10:24:06 +0900

    [level 2] Title: 요격 시스템, Time: 605.41 ms, Memory: 191 MB -BaekjoonHub

---
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+
+class Solution {
+    public int solution(int[][] targets) {
+        Arrays.sort(targets, ((o1, o2) -> o1[1] - o2[1]));
+
+        int answer = 1;
+        int idx = 0;
+        int endX = targets[0][1];
+
+        for (int[] target : targets) {
+            idx++;
+            int startX = target[0];
+            if (endX > startX) continue;
+            endX = target[1];
+            answer++;
+        }
+
+        return answer;
+    }
+}
