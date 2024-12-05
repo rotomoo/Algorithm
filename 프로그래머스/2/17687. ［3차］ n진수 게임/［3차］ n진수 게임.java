b7ff8847b1f7708ef61ecb7a89e093ef8b7ab9f3
손병일 <30739181+rotomoo@users.noreply.github.com>
2024-12-05 10:00:39 +0900

    [level 2] Title: [3차] n진수 게임, Time: 427.17 ms, Memory: 411 MB -BaekjoonHub

---
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -0,0 +1,20 @@
+import java.math.BigInteger;
+
+class Solution {
+    public String solution(int n, int t, int m, int p) {
+        String answer = "";
+        String tmp = "";
+        int cnt = 0;
+        int findLen = t * m;
+        int num = 0;
+        while (findLen >= tmp.length()) {
+            tmp += new BigInteger(String.valueOf(num++)).toString(n);
+        }
+        while (cnt != t) {
+            answer += tmp.charAt(p-1);
+            p += m;
+            cnt++;
+        }
+        return answer.toUpperCase();
+    }
+}
