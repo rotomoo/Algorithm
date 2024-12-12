798fa7c868ea8ffa641e6e3c0e240f30c4a3c1b9
손병일 <30739181+rotomoo@users.noreply.github.com>
2024-12-12 09:59:05 +0900

    [Silver III] Title: 주유소, Time: 380 ms, Memory: 35904 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long[] arr= new long[n-1];
+        long[] arr2= new long[n];
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        for (int i=0; i<n-1; i++) {
+            arr[i]=Long.parseLong(st.nextToken());
+        }
+        long min=Integer.MAX_VALUE;
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<n; i++) {
+            arr2[i]=Long.parseLong(st.nextToken());
+            min=Math.min(min,arr2[i]);
+            arr2[i]=min;
+        }
+        long answer=0;
+        for (int i=0; i<n-1; i++) {
+            answer+=arr[i]*arr2[i];
+        }
+        System.out.println(answer);
+    }
+}
