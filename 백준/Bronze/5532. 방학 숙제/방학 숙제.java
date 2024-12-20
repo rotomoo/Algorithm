d691274331035ed3e3711eec94cbfa2000c7b077
손병일 <30739181+rotomoo@users.noreply.github.com>
2024-12-20 14:00:22 +0900

    [Bronze IV] Title: 방학 숙제, Time: 116 ms, Memory: 14000 KB -BaekjoonHub

---
+++ 백준/Bronze/5532. 방학 숙제/방학 숙제.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int l = Integer.parseInt(br.readLine());
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        int d = Integer.parseInt(br.readLine());
+        System.out.println(l-(int)Math.max(Math.ceil((double)a/c),Math.ceil((double)b/d)));
+    }
+}
