94089fb6f0cb8a65e77dd27b2807da60f6a7f2dc
손병일 <30739181+rotomoo@users.noreply.github.com>
2024-12-27 10:17:01 +0900

    [Bronze III] Title: 좋은 암호, Time: 552 ms, Memory: 77996 KB -BaekjoonHub

---
+++ 백준/Bronze/2061. 좋은 암호/좋은 암호.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BigInteger k = new BigInteger(st.nextToken());
+        int l = Integer.parseInt(st.nextToken());
+        int[] ch = new int[l+1];
+        for (int i=2; i<l; i++) {
+            if (ch[i]==1) continue;
+            if (k.mod(BigInteger.valueOf(i)).toString().equals("0")) {
+                System.out.println("BAD"+" "+i);
+                System.exit(0);
+            }
+            for (int j = i; j<=l; j+=i) {
+                ch[j]=1;
+            }
+        }
+        System.out.println("GOOD");
+    }
+}
