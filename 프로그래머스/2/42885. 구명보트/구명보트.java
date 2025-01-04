d611d2e5971579326f10420366952d9e622e3691
손병일 <30739181+rotomoo@users.noreply.github.com>
2025-01-04 20:05:29 +0900

    [level 2] Title: 구명보트, Time: 10.77 ms, Memory: 58.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        int lt = 0;
+        int rt = people.length-1;
+        while (lt <= rt) {
+            if (people[lt] + people[rt] <= limit) {
+                lt++;
+            }
+            answer++;
+            rt--;
+        }
+        return answer;
+    }
+}
