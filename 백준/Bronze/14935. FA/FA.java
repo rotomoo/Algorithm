945fe55f9d76b9df7da33fc226c54dfe174b9811
손병일 <30739181+rotomoo@users.noreply.github.com>
2025-01-13 10:00:31 +0900

    [Bronze IV] Title: FA, Time: 124 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/14935. FA/FA.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String n = br.readLine();
+        int cnt=0;
+        while (cnt < 9) {
+            String tmp = String.valueOf((n.charAt(0)-'0') * n.length());
+            if (tmp.length()==1) {
+                System.out.println("FA");
+                break;
+            }
+            n = tmp;
+            cnt++;
+        }
+    }
+}
