20938a2226593dc67d04039bc43467a5525da8d3
손병일 <30739181+rotomoo@users.noreply.github.com>
2025-01-21 10:29:08 +0900

    [Bronze III] Title: 체스판 조각, Time: 124 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/3004. 체스판 조각/체스판 조각.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        if (n%2==0) System.out.println((int)Math.pow(n/2+1,2));
+        else System.out.println((n/2+1)*(n/2+2));
+    }
+}
