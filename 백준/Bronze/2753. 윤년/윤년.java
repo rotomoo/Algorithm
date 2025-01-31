3f3ee1a017413aba01c8d47c00bd694058d737ef
손병일 <30739181+rotomoo@users.noreply.github.com>
2025-01-31 09:18:01 +0900

    [Bronze V] Title: 윤년, Time: 164 ms, Memory: 16316 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(br.readLine());
+        if (x%4 == 0 && (x%100 != 0 || x%400 == 0)) {
+                System.out.println(1);
+                System.exit(0);
+        }
+        System.out.println(0);
+    }
+}
