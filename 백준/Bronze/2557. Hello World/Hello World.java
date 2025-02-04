2cbb353fa06be0aef46d532af5c2b8f820bdaa0c
손병일 <30739181+rotomoo@users.noreply.github.com>
2025-02-04 10:05:56 +0900

    [Bronze V] Title: Hello World, Time: 116 ms, Memory: 14044 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
+    }
+
+}
