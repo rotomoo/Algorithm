32a62aaf5815123ad6b2b234c3bc506a9a59a4c6
손병일 <30739181+rotomoo@users.noreply.github.com>
2025-02-16 17:50:46 +0900

    [level 2] Title: 모음 사전, Time: 9.62 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+class Solution {
+    static String[] arr = {"A", "E", "I", "O", "U"};
+    static ArrayList<String> list = new ArrayList<>();
+    static String wordTmp;
+    static boolean flag = true;
+
+    public static void DFS(int L, String s) {
+        if (L==6) return;
+        list.add(s);
+        if (flag) {
+            for (int i = 0; i < 5; i++) {
+                DFS(L + 1, s + arr[i]);
+            }
+        }
+    }
+
+    public int solution(String word) {
+        wordTmp = word;
+        DFS(0, "");
+        return list.indexOf(word);
+    }
+}
