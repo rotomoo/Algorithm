eeb80da4576181f8ac6f34f4669a9006ee39a1a0
손병일 <30739181+rotomoo@users.noreply.github.com>
2025-02-22 13:38:03 +0900

    [level 2] Title: 괄호 회전하기, Time: 56.56 ms, Memory: 86.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Stack;
+
+class Solution {
+
+    public static boolean isCheck(String tmp) {
+        HashMap<Character, Character> map = new HashMap<>();
+        Stack<Character> stack = new Stack<>();
+        map.put('}', '{');
+        map.put(']', '[');
+        map.put(')', '(');
+        for (char x : tmp.toCharArray()) {
+            if (!map.containsKey(x)) {
+                stack.push(x);
+            }
+            else {
+                if (stack.isEmpty() || stack.pop() != map.get(x)) return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public int solution(String s) {
+        int answer = 0;
+        for (int i = 0; i < s.length(); i++) {
+            String tmp = s.substring(i) + s.substring(0, i);
+            if (isCheck(tmp)) answer++;
+        }
+        return answer;
+    }
+}
