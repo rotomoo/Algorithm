c2f1565a6576e8b075b68746326e6fcbe230abdf
손병일 <30739181+rotomoo@users.noreply.github.com>
2025-02-25 10:19:28 +0900

    [level 1] Title: 신규 아이디 추천, Time: 17.78 ms, Memory: 85.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String new_id) {
+        new_id = new_id.toLowerCase();
+        new_id = new_id.replaceAll("[^-_.a-z0-9]", "");
+        new_id = new_id.replaceAll("[.]{2,}", ".");
+        new_id = new_id.replaceAll("^[.]|[.]$", "");
+        if(new_id.equals("")) new_id = "a";
+        if(new_id.length() >= 16) new_id = new_id.substring(0,15).replaceAll("^[.]|[.]$", "");
+        while(new_id.length() < 3) new_id += new_id.charAt(new_id.length()-1);
+        return new_id;
+    }
+}
