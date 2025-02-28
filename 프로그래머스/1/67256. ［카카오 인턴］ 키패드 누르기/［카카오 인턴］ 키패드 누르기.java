f30d3a8758dee4f4e8c1740afb9d144937c40d0b
손병일 <30739181+rotomoo@users.noreply.github.com>
2025-02-28 17:00:20 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 5.87 ms, Memory: 65.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -0,0 +1,47 @@
+class Solution {
+    public String solution(int[] priorities, String hand) {
+        String answer = "";
+        int[] lH={3,0};
+        int[] rH={3,2};
+        for(int x : priorities) {
+            if (x==1 || x==4 || x==7) {
+                answer+='L';
+                lH=check(x);
+            }
+            else if (x==3 || x==6 || x==9) {
+                answer+='R';
+                rH=check(x);
+            }
+            else {
+                int[] tmpxy=check(x);
+                if (pri(tmpxy,lH,rH,hand)=='R') {
+                    answer+='R';
+                    rH=tmpxy;
+                }
+                else {
+                    answer+='L';
+                    lH=tmpxy;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public int[] check(int x) {
+        int[][] numxy={{3,1},{0,0},{0,1},{0,2},{1,0},{1,1},{1,2},{2,0},{2,1},{2,2}};
+        return numxy[x];
+    }
+
+    public char pri(int[] tmpxy, int[] lH, int[] rH,String hand) {
+        char tmp=' ';
+        int ldis=Math.abs(lH[0]-tmpxy[0])+Math.abs(lH[1]-tmpxy[1]);
+        int rdis=Math.abs(rH[0]-tmpxy[0])+Math.abs(rH[1]-tmpxy[1]);
+        if (ldis>rdis) tmp='R';
+        else if (ldis<rdis) tmp='L';
+        else {
+            if (hand.equals("right")) tmp='R';
+            else tmp='L';
+        }
+        return tmp;
+    }
+}
