f3ab1c288818ca8d1ffda5ecaf22a0cd933fd77d
손병일 <30739181+rotomoo@users.noreply.github.com>
2025-03-02 15:53:15 +0900

    [level 2] Title: 튜플, Time: 107.70 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Solution {
+    public int[] solution(String s) {
+        String[] split = s.split("},\\{");
+        ArrayList<String> list = new ArrayList<>();
+        for (String x : split) {
+            x = x.replaceAll("[{}]", "");
+            list.add(x);
+        }
+        Collections.sort(list, (o1, o2) -> o1.length()-o2.length());
+        ArrayList<Integer> answer = new ArrayList<>();
+        for (String x : list) {
+            String[] split2 = x.split(",");
+            for (String st : split2) {
+                int tmp = Integer.parseInt(st);
+                if (!answer.contains(tmp)) answer.add(tmp);
+            }
+        }
+        return answer.stream().mapToInt(i -> i).toArray();
+    }
+}
