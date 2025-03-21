a081111994edbe9170749fcf9e0deae4a0613149
손병일 <30739181+rotomoo@users.noreply.github.com>
2025-03-21 10:09:38 +0900

    [level 2] Title: 더 맵게, Time: 1690.08 ms, Memory: 128 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,18 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+         PriorityQueue<Integer> pqScov = new PriorityQueue<>();
+         for (int s: scoville) {
+             pqScov.add(s);
+         }
+
+         int cnt = 0;
+         while (pqScov.size() > 1 && pqScov.peek() < K) {
+             pqScov.add(pqScov.remove() + pqScov.remove() * 2);
+             cnt++;
+         }
+
+         return pqScov.peek() >= K ? cnt : -1;
+    }
+}
