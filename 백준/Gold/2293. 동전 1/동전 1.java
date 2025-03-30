4a84a1e07bd14767e83d66740a23b36ea88718dc
손병일 <30739181+rotomoo@users.noreply.github.com>
2025-03-30 19:35:41 +0900

    [Gold IV] Title: 동전 1, Time: 108 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        int[] dy = new int[k+1];
+        for(int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        dy[0]=1;
+        for (int i=0; i<n; i++) {
+            for (int j=arr[i]; j<=k; j++) {
+                dy[j] += dy[j-arr[i]];
+            }
+        }
+        System.out.println(dy[k]);
+    }
+}
