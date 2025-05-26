5a159ecb49ae69edd9a2c9cdaf1c9394db44d9c3
손병일 <30739181+rotomoo@users.noreply.github.com>
2025-05-26 13:52:06 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 45.11 ms, Memory: 88.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] s1 = s.split(" ");
+        Arrays.sort(s1, (o1, o2) ->{
+            return Integer.parseInt(o1) - Integer.parseInt(o2);
+        });
+        answer += s1[0] + " " + s1[s1.length-1];
+        return answer;
+    }
+}
