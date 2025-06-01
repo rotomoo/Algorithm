eb98f03574a27c6c8ecbe9599f7cc08061c4eee1
손병일 <30739181+rotomoo@users.noreply.github.com>
2025-06-01 12:25:01 +0900

    [level 2] Title: 3 x n 타일링, Time: 38.31 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long solution(int n) {      
+        long [] dp = new long [n+1];
+        dp[2] = 3;
+
+        for(int i = 4; i <= n; i+=2){
+            dp[i] = dp[i-2]*dp[2] + 2;
+
+            for(int j = 2; j <= i-4; j+=2) {
+                dp[i] += dp[j]*2;    
+            }
+
+            dp[i] = dp[i]%1000000007;
+        }
+
+        return dp[n];
+
+    }
+}
